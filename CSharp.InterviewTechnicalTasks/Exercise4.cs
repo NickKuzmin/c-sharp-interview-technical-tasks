06a70b7005708ef6ebbbf8bd532941bb750c30e1
Nikita Kuzmin <kuzmin.nick@rambler.ru>
2021-12-22 09:31:13 +0300

    Commit

---
+++ CSharp.InterviewTechnicalTasks/Exercise4.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace CSharp.InterviewTechnicalTasks.Exercise4
+{
+    public class Program
+    {
+        public class A<T>
+        {
+            public static int Count { get; set; }
+            public A() { Count++; }
+        }
+
+        public static void Internal(string[] args)
+        {
+            // var A1 = new A<object>();
+            var A2 = new A<string>();
+            var A3 = new A<int>();
+            Console.WriteLine(A<object>.Count); // 1
+        }
+    } 
+}
---
+++ CSharp.InterviewTechnicalTasks/Exercise5.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace CSharp.InterviewTechnicalTasks.Exercise5
+{
+    public class Bus
+    {
+        static int RouteNumber { get; set; }
+
+        static Bus()
+        {
+            RouteNumber = 1;
+        }
+
+        public Bus(int routeNum)
+        {
+            if (RouteNumber == 1)
+            {
+                RouteNumber = routeNum;
+                Console.WriteLine("Bus #{0} is created.", RouteNumber);
+            }
+        }
+
+        public void Drive()
+        {
+            Console.WriteLine("Bus #{0} drive.", RouteNumber);
+        }
+    }
+
+    public class Program
+    {
+        public static void Internal(string[] args)
+        {
+            Bus bus1 = new Bus(71); // 71
+            Bus bus2 = new Bus(72); // 71
+
+            bus1.Drive(); // 71
+            bus2.Drive(); // 71
+        }
+    } 
+}
